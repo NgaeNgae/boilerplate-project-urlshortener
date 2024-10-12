fa96423157df60bf92a4fd5f5b051e87a77a3b2d
Naing Htet Htun <axel8102005@gmail.com>
2024-10-12 02:16:04 +0000

    19

--- index.js
+++ index.js
@@ -1,24 +1,69 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-
+const dns = require("dns");
+const url = require("url");
+const fs = require("fs");
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+function dataManagement(action, input) {
+  let filepath = "./public/data.json";
+  if (!fs.existsSync(filepath)) {
+    fs.writeFileSync(filepath, JSON.stringify([], null, 2));
+  }
+  let file = fs.readFileSync(filepath);
+  let data = JSON.parse(file);
+  if (action === "add") {
+    let add = {
+        original_url : input,
+        short_url : data.length + 1,
+      }
+    data.push(add);
+     fs.writeFileSync(filepath, JSON.stringify(data, null, 2));
+    return add;
+  } else {
+    return data;
+  }
+}
 
-app.listen(port, function() {
+app.post("/api/shorturl", function (req, res) {
+  let input = req.body.url;
+  dns.lookup(url.parse(input).hostname, async (err, address) => {
+    if (!address) {
+      res.json({ error: "Invalid URL" });
+    } else {
+      let data = dataManagement("add", input);
+      res.json({
+        original_url: data.original_url,
+        short_url: data.short_url,
+      });
+    }
+  });
+});
+app.get("/api/shorturl/:shorturl", (req, res) => {
+  let input = Number(req.params.shorturl);
+  console.log(input);
+  let all_data = dataManagement("get");
+  let short = all_data.map(d => d.short_url);
+  let data = short.includes(input);
+  if (data) {
+    data_found = all_data[short.indexOf(input)];
+    res.redirect(data_found.original_url);
+  } else {
+    res.json({ data: "No matching data", short: input });
+  }
+});
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
